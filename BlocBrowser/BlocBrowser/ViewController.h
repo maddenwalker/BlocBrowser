b29a2f1fe2c6c122dea01c322d74d0653be75351
Ryan Walker <ryan@maddenwalker.com>
2015-10-01 15:41:12 -0700

    Initial Commit

---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Ryan Walker on 10/1/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Ryan Walker on 10/1/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
