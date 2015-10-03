e76a428b9d3fb92d8e0818a11c03c20dbd77304a
Ryan Walker <ryan@maddenwalker.com>
2015-10-02 20:47:10 -0700

    Checkpoint 23 BLocBrowser, Completed

--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -7,21 +7,228 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
+@interface ViewController () <WKNavigationDelegate, UITextFieldDelegate>
+
+@property (strong, nonatomic) WKWebView *webView;
+@property (strong, nonatomic) NSData *webData;
+@property (strong, nonatomic) UITextField *textField;
+
+@property (weak, nonatomic) UIButton *backButton;
+@property (weak, nonatomic) UIButton *forwardButton;
+@property (weak, nonatomic) UIButton *stopButton;
+@property (weak, nonatomic) UIButton *reloadButton;
+@property (strong, nonatomic) UIActivityIndicatorView *activityIndicator;
 
 @end
 
+#pragma mark - UIViewController
+
 @implementation ViewController
+
+- (void)loadView {
+    UIView *mainView = [UIView new];
+    
+    //create webview
+    self.webView = [[WKWebView alloc] init];
+    self.webView.navigationDelegate = self;
+    
+    //create text field
+    self.textField = [[UITextField alloc] init];
+    self.textField.keyboardType = UIKeyboardTypeURL;
+    self.textField.returnKeyType = UIReturnKeyDone;
+    self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
+    self.textField.delegate = self;
+    
+    //create buttons at end of page
+    self.backButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.backButton setEnabled:NO];
+    
+    self.forwardButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.forwardButton setEnabled:NO];
+    
+    self.stopButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.stopButton setEnabled:NO];
+    
+    self.reloadButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.reloadButton setEnabled:NO];
+    
+    [self.backButton setTitle:NSLocalizedString(@"Back", "Back Command") forState:UIControlStateNormal];
+    [self.backButton addTarget:self.webView action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.forwardButton setTitle:NSLocalizedString(@"Forward", "Forward Command") forState:UIControlStateNormal];
+    [self.forwardButton addTarget:self.webView action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.stopButton setTitle:NSLocalizedString(@"Stop", "Stop Command") forState:UIControlStateNormal];
+    [self.stopButton addTarget:self.webView action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.reloadButton setTitle:NSLocalizedString(@"Reload", "Reload Command") forState:UIControlStateNormal];
+    [self.reloadButton addTarget:self.webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+    
+    //load all of the views as subview
+    for (UIView *viewToAdd in @[self.webView, self.textField, self.forwardButton, self.backButton, self.stopButton, self.reloadButton]) {
+        [mainView addSubview:viewToAdd];
+    }
+    
+    self.view = mainView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.activityIndicator];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    static const CGFloat itemHeight = 50;
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - ( 2 * itemHeight );
+    CGFloat buttonWidth = CGRectGetWidth(self.view.bounds)  / 4 ;
+    
+    self.textField.frame = CGRectMake(0, 0, width, itemHeight);
+    self.webView.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
+    
+    CGFloat currentButtonX = 0;
+    
+    for (UIButton *thisButton in @[self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        thisButton.frame = CGRectMake(currentButtonX, CGRectGetMaxY(self.webView.frame), buttonWidth, itemHeight);
+        currentButtonX += buttonWidth;
+    }
+    
 }
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    
+    NSString *URLString = textField.text;
+    NSURL *URL = [NSURL URLWithString:URLString];
+    NSString *correctURLScheme = @"https";
+    
+    if ( ![URL.scheme isEqualToString: correctURLScheme]) {
+        if ( [URL.scheme isEqualToString:@"http"] ) {
+            
+            NSRange locationOfNonSecureURL = [URLString rangeOfString:@"http"];
+            URLString = [URLString stringByReplacingCharactersInRange:locationOfNonSecureURL withString:correctURLScheme];
+            URL = [NSURL URLWithString:URLString];
+            
+        } else {
+            
+            URL = [NSURL URLWithString:[NSString stringWithFormat:@"%@://%@", correctURLScheme, URLString]];
+            
+        }
+    }
+    
+    if (URL) {
+        NSURLSession *session = [NSURLSession sharedSession];
+        NSURLSessionDataTask *task = [session dataTaskWithURL:URL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            if (!error) {
+                [self.webView loadData:data MIMEType:@"text/html" characterEncodingName:@"UTF-8" baseURL:URL];
+            } else {
+                NSLog(@"%@", [error localizedDescription] );
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self showUserError:error];
+                });
+            }
+        }];
+        [task resume];
+    }
+    
+    return NO;
+}
+
+#pragma mark - WKNavigationDelegate
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    [self updateButtonsAndTitle];
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    [self updateButtonsAndTitle];
+}
+
+- (void) webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    
+    [self webView:webView didFailProvisionalNavigation:navigation withError:error];
+    
+}
+
+- (void) webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    
+    if (error.code != NSURLErrorCancelled) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Error", @"Error")
+                                                                       message:[error localizedDescription]
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK",nil)
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:nil];
+        
+        [alert addAction:okAction];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+        
+        [self updateButtonsAndTitle];
+        
+    }
+}
+
+#pragma mark - Alert Method
+
+- (void) showUserError:(NSError *)error {
+    
+    if (error.code != NSURLErrorCancelled) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Error", @"Error")
+                                                                       message:[error localizedDescription]
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK",nil)
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:nil];
+        
+        [alert addAction:okAction];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+
+    }
+    
+    [self updateButtonsAndTitle];
+}
+
+#pragma mark - Miscellaneous
+- (void) updateButtonsAndTitle {
+    
+    NSString *webPageTitle = [self.webView.title copy];
+    if ([ webPageTitle length ]) {
+        self.title = webPageTitle;
+    } else {
+        self.title = self.webView.URL.absoluteString;
+    }
+    
+    if (self.webView.isLoading) {
+        [self.activityIndicator startAnimating];
+    } else {
+        [self.activityIndicator stopAnimating];
+    }
+    
+    self.backButton.enabled = [self.webView canGoBack];
+    self.forwardButton.enabled = [self.webView canGoForward];
+    self.stopButton.enabled = self.webView.isLoading;
+    self.reloadButton.enabled = !self.webView.isLoading;
+    
+}
+
 
 @end
